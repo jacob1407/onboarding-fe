9d8de81ac86dd67504aa75de885927c6a644b747
Jacob Kim <jacob1998.kim@gmail.com>
2025-06-03 21:51:32 +1000

    Fix environment variable

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080"
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080"
 
 async function request<T>(url: string, options: RequestInit): Promise<T> {
+    console.log("process.env.NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL)
     const token = localStorage.getItem("access_token")
     const res = await fetch(`${BASE_URL}${url}`, {
         headers: {
